e2e2bddca3840139680eafe2afa32fff1d68eb86
umangsontakke <50995664+umangsontakke@users.noreply.github.com>
2020-08-12 20:33:25 +0530

    Update userregistration.sh

--- userregistration.sh
+++ userregistration.sh
@@ -1,1 +1,11 @@
-echo "wellcome to pattern programming"
+#!/bin/bash -x
+
+read -p "enter  email =" email 
+
+pattern="/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
+if [[ $email   =~ $pattern ]]
+then
+echo "email   is valid"
+else
+echo "email is   invalid"
+fi
